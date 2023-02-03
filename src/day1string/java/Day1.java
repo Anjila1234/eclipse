947e609850087b772549517804fc247c2255d167
Anjila1234 <anzelakatuwal@gmail.com>
2023-02-02 20:07:31 -0600

    firstcommit

---
+++ src/day1string/java/Day1.java
@@ -0,0 +1,38 @@
+package day1string.java;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String subject = "I am learning java";
+		String subject1 = " I am learning python";
+		
+		System.out.println(subject);
+		System.out.println(subject1);
+		
+		
+		
+		System.out.println("Hy"+3);
+		System.out.println(2+3);
+		System.out.println(3+ "hello");
+		
+		String greet = "I am learning QA";
+		int a1 = greet.length();
+		System.out.println(a1);
+		System.out.println(greet.length());
+		
+		
+		int a2 = 40;
+		System.out.println(a2);
+		System.out.println("a2");
+		
+		
+String first = "QA";
+String second = "Learning";
+String three = first.concat(first);
+System.out.println(three);
+
+	}
+
+}
