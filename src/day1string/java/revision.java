6c7349c0cf2563a949923333909382d9a231d073
Anjila1234 <anzelakatuwal@gmail.com>
2023-02-11 15:30:11 -0600

    message

--- src/day1string/java/revision.java
+++ src/day1string/java/revision.java
@@ -1,5 +1,7 @@
 package day1string.java;
 
+import java.util.Arrays;
+
 import two.String;
 
 public class revision {
@@ -115,26 +117,259 @@ public class revision {
 //	}
 //	System.out.println(count);
 //	
-	
-	String firstName = "chinmay";
-	int count1 = 0;
-	
-	
-	for(int i = 0  ; i < firstName.length() ; i++) {
-//		System.out.println(firstName.charAt(i));
-		
-		if(firstName.charAt(i) == 'a' || firstName.charAt(i) == 'e' || firstName.charAt(i) == 'i' || firstName.charAt(i) == 'o' || firstName.charAt(i) == 'u') {
-			count1 = count1 + 1;
-		}
+//	
+//	String firstName = "chinmay";
+//	int count1 = 0;
+//	
+//	
+//	for(int i = 0  ; i < firstName.length() ; i++) {
+////		System.out.println(firstName.charAt(i));
+//		
+//		if(firstName.charAt(i) == 'a' || firstName.charAt(i) == 'e' || firstName.charAt(i) == 'i' || firstName.charAt(i) == 'o' || firstName.charAt(i) == 'u') {
+//			count1 = count1 + 1;
+//		}
+//			
+//		
+//	}
+//	
+//	System.out.println(count1);
 			
+	
+		
+
+	String abc = "Anjila Bhagwati";
+	
+	String def = abc.substring(5);
+	System.out.println(def);
+	
+	String ghi = abc.substring (0, 5);
+	System.out.println(ghi);
+	
+	String anz = "Hi";
+	String bhg = "How";
+	String chu = "are you ?";
+	
+	String pra = String.join(" ", anz, bhg, chu);
+	System.out.println(pra);
+	
+	String are = String.join("@", pra, anz);
+	System.out.println(are);
+	
+	String a1 = "Anjila";
+	String bl = "Bhagwati";
+	String c1 = "Chunu";
+	
+	String e1 = String.join("-" , a1, bl, c1  );
+	System.out.println(e1);
+	
+	//K a t h m a n d u
+	//0 1 2 3 4 5 6 7 8 
+	String city = "Kathmandu";
+	int  q = city.indexOf('h');
+	System.out.println(q);
+	
+	int p = city.indexOf("m",1);
+	System.out.println(p);
+	
+	for(int i = 0; i< city.length(); i++) {
+		if( city.charAt(i)== 'a') {
+			System.out.println(i);
+		}
+		
 		
 	}
+	String a3 = " I am learning java ";
+	String a4 = a3.trim();
+	System.out.println(a3.length());
+	System.out.println(a4.length());
 	
-	System.out.println(count1);
-			
-	}}
+	String a5 = a4.replace(" ", "");
+	System.out.println(a5);
+	
+	String a6 = a4.replace("learning", "studying");
+	System.out.println(a6);
+	
+	String a7 = a4.replaceAll(" I am learning java ", "QA");
+	System.out.println(a7);
+	
+	String a8 = "I am learning java and I love learning java";
+	String a9 = a8.replaceAll("learning", "studying");
+	System.out.println(a9);
+	
+	String a10 = " I am learning QA";
+	String [] a11 = a10.split("a");
+	System.out.println(a11);
+	System.out.println(Arrays.toString(a11));
+	
+	for(String m:a11) {
+		System.out.println(m);
+	}
+	
+	String a12 = "Amazing";
+	String a13 ="";
+	
+	boolean a14 = a12.isEmpty();
+	System.out.println(a14);
+	boolean a15 = a13.isEmpty();
+	System.out.println(a15);
+	
+	int [][]a = new int [1][2];
+	
+	
+	int [][] b1 = {
+			{1,22,333},
+			{4444,55555,666666,7777777},
+			{88888888}		
+	};
+	System.out.println(b1[0]);
+	System.out.println(b1[0][1]);
+	System.out.println(b1[1][2]);
+	
+	int [][] a20 = {
+			{ 5, 6, 7},
+			{7, 8, 9 },
+			{10, 11, 12, 13} };
+	
+	System.out.println(a20[1][1]);
+	
+for(int i  = 0; i < a20.length; i++) {
+//	System.out.println(i);
+	System.out.println(a20[i].length);
+}
+//int []man = a20[i];
+//for(int k = 0; k < man.length; i++) {
+//	System.out.println(k);
+//}
+
+int [][] numbers = {
+		{-1,-2,-3},      // 0
+		{-11,-22,-33},  // 1
+		{-44,-55,-66}	// 2
+};
+
+for(int i = 0 ; i < numbers.length ; i++) {
+	//System.out.println(i);
+	//System.out.println(numbers[i]);
+	//System.out.println(Arrays.toString(numbers[i]));
+	int rows [] = numbers[i];
+	for(int j = 0 ; j < rows.length ; j++) {
+		System.out.println(rows[j]);
+	}
+}
+
+
+int [][] num = {
+		{ 5, 10 , 15},
+		{15, 20, 30},
+		{30,40,50}};
+
+
+for (int i = 0; i< num.length; i++) {
+	System.out.println(i);
+	System.out.println(num[i]);
+	System.out.println(Arrays.toString(num[i]));
+	
+	int row1 [] = num[i];
+	for(int m =0; m < row1.length; m++) {
+		System.out.println(row1[m]);}
+	
+
+
+int [][] num1 = {
+		{1, 2, 3, 4},
+		{2, 4,  6, 8},
+		{9, 10}};
+
+System.out.println(num1[0][3]);
+System.out.println(num1[2][1]);
+
+for (int v = 0; v< num1.length; v++) {
+//	System.out.println(v);
+//	System.out.println(num1[v]);
+	System.out.println(Arrays.toString(num1[v]));
+	
+	int row2 [] = num1[v];
+	for(int u = 0; u < row2.length; u++ ) {
+		System.out.println(row2[u]);
+	}
+}
+}
+// For each loop
+int num2 []= {1,2,3,4};
+for (int s1:num2 ) {
+	System.out.println(s1);
+	
+	int num3 [] = { 5, 6,7,8};
+	for (int s2:num3) {
+		System.out.println(s2);
 		
-
+		//for loop
+		
+		int num4 [][] = {
+				{22, 44, 55, 66},
+				{55, 66, 67, 89},
+				{55, 77, 88, 90}
+		};
+		
+		for(int i = 0; i< num4.length; i++) {
+			System.out.println(i);
+			System.out.println(Arrays.toString(num4[i]));
+		}
+	}
 	
+	int num5 [][]= {
+			{3, 4, 5, 6},
+			{8, 12, 14},
+			{6, 7, 8, 9}};
+	
+	for (int l =0; l< num5.length; l++) {
+	System.out.println(l);
+	System.out.println(Arrays.toString(num5[l]));	
+	
+	int row4 []= num5[l];
+	for(int i =0; i<row4.length; i++) {
+		System.out.println(row4[i]);
+	}
+}}
+
+String vegetables2 = "onion potato cauliflower";
+String available = "onion";
+if(vegetables2.contains(available)){
+	System.out.println("yes");	
+}
+else { 
+	System.out.println("No");}
+
+String result10 = vegetables2.contains("onion")?"yes":"No";
+System.out.println(result10);
+
+String fruits1 = " apple banana mango pomegranate";
+String exist = "banana";
+if(fruits1.contains(exist)) {
+	System.out.println("available");
+}
+else {
+	System.out.println("not available");
+}
+
+String result12 = fruits1.contains("banana")? "It is available": "It is not available";
+System.out.println(result12);
 
 
+String food = "ricepudding";
+int taste = food.indexOf('d');
+System.out.println(taste);
+
+int smell = food.indexOf('i', 5);
+System.out.println(smell);
+int count5 =0;
+for(int i =0; i< food.length(); i++) {
+	if (food.charAt(i) == 'p')
+	System.out.println(i);
+	count5 = count5 + 1;
+}
+System.out.println(count5);
+
+
+
+}}
